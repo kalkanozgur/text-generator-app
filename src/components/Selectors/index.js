741ebdcdb3f651693fa17fde2f09dcf1788f83d3
kalkanozgur <ahmetozgurkalkan@gmail.com>
2022-10-05 19:06:00 +0300

    reduxjs/toolkit tailwind && base components

---
+++ src/components/Selectors/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Selectors() {
+	return <div>Selectors</div>;
+}
+
+export default Selectors;
---
+++ src/components/TextContent/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TextContent() {
+	return <div>TextContent</div>;
+}
+
+export default TextContent;
